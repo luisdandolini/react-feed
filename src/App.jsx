2330502abf6cb28dec36a8c48d6a3c626c4bf851
Luís Fernando Dandolini Duarte <luisfernandodandoliniduarte@gmail.com>
2024-03-05 18:03:09 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { Post } from "./Post"
+
+function App() {
+
+  return (
+    <>
+      <Post 
+        author="Diego Fernandes" 
+        content="Conteúdo"
+      />
+    </>
+  )
+}
+
+export default App
