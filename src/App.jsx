7da0b37b4f0816f65f7aac19932deb5ac2055091
Luís Fernando Dandolini Duarte <luisfernandodandoliniduarte@gmail.com>
2024-03-06 21:21:49 -0300

    feat: created header

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
 import { Post } from "./Post"
+import { Header } from "./components/Header";
+import './global.css';
 
 function App() {
 
   return (
     <>
+      <Header />
       <Post 
         author="Diego Fernandes" 
         content="Conteúdo"
