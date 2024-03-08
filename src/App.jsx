41e0ef08eb51bbd2a19767987c337cdbbe9724c5
Luís Fernando Dandolini Duarte <luisfernandodandoliniduarte@gmail.com>
2024-03-08 18:27:05 -0300

    feat: create sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,25 @@
 import { Post } from "./Post"
 import { Header } from "./components/Header";
 import './global.css';
+import styles from './App.module.css';
+import { Sidebar } from "./components/Sidebar";
 
 function App() {
 
   return (
     <>
       <Header />
-      <Post 
-        author="Diego Fernandes" 
-        content="Conteúdo"
-      />
+
+      <div className={styles.wrapper}>
+        <Sidebar />
+
+        <main>
+          <Post 
+            author="Diego Fernandes" 
+            content="Conteúdo"
+          />
+        </main>
+      </div>
     </>
   )
 }
---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,27 @@
+import styles from './Sidebar.module.css';
+import { PencilLine } from "@phosphor-icons/react";
+
+export const Sidebar = () => {
+  return (
+    <aside className={styles.sidebar}>
+      <img 
+        className={styles.cover}
+        src="https://images.unsplash.com/photo-1604964432806-254d07c11f32?q=80&w=500&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" 
+      />
+
+      <div className={styles.profile}>
+        <img src="https://github.com/luisdandolini.png" />
+
+        <strong>Luís Dandolini</strong>
+        <span>Web Developer</span>
+      </div>
+
+      <footer>
+        <a href="#">
+          <PencilLine size={20} />
+          Editar seu perfil
+        </a>
+      </footer>
+    </aside>
+  )
+}
